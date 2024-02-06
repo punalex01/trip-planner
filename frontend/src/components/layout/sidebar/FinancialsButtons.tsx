42ca4ff83a04d51fa4cc71289d9bbd4cfec47985
Alex Pun <punalex01@gmail.com>
2024-02-05 22:41:20 -0800

    feat: add sidebar hover logic for individual/group buttons

--- frontend/src/components/layout/sidebar/FinancialsButtons.tsx
+++ frontend/src/components/layout/sidebar/FinancialsButtons.tsx
@@ -1,6 +1,7 @@
 import { ChevronDown, ChevronUp, CircleDollarSign, User, Users } from 'lucide-react';
 import { useState, FC } from 'react';
 import { useAppContext } from 'src/context/AppContext';
+import { GroupViewType } from 'src/context/types';
 import { Module } from 'src/interfaces/Module';
 
 interface FinancialsButtonsProps {
@@ -12,8 +13,18 @@ export const FinancialsButtons: FC<FinancialsButtonsProps> = ({ isSidebarCollaps
   const [isCollapsed, setCollapsed] = useState(true);
   const [{ currentTripModule }, { setCurrentModule, setGroupViewType }] = useAppContext();
   const currSelectedModule = currentTripModule.module;
+  const groupView = currentTripModule.viewType;
 
-  const bg = currSelectedModule && currSelectedModule.id === module.id ? ' bg-eggplant/20' : ' hover:bg-eggplant/20';
+  const moduleBackground =
+    currSelectedModule && currSelectedModule.id === module.id ? ' bg-eggplant/20' : ' hover:bg-eggplant/20';
+
+  const getGroupViewButtonBackground = (viewType: GroupViewType) => {
+    if (currSelectedModule === null || currSelectedModule.id !== module.id || groupView === null) {
+      return ' hover:bg-eggplant/20';
+    } else if (groupView === viewType) {
+      return ' bg-eggplant/20';
+    }
+  };
 
   if (!isCollapsed) {
     return (
@@ -21,7 +32,7 @@ export const FinancialsButtons: FC<FinancialsButtonsProps> = ({ isSidebarCollaps
         onClick={() => setCollapsed(!isCollapsed)}
         className={
           'flex grow h-10 w-full px-2 space-x-2 rounded-lg items-center hover:cursor-pointer overflow-clip line-clamp-1' +
-          bg
+          moduleBackground
         }
       >
         <CircleDollarSign />
@@ -36,7 +47,7 @@ export const FinancialsButtons: FC<FinancialsButtonsProps> = ({ isSidebarCollaps
           onClick={() => setCollapsed(!isCollapsed)}
           className={
             'flex grow h-10 w-full px-2 space-x-2 rounded-lg items-center hover:cursor-pointer overflow-clip line-clamp-1' +
-            bg
+            moduleBackground
           }
         >
           <CircleDollarSign />
@@ -45,20 +56,26 @@ export const FinancialsButtons: FC<FinancialsButtonsProps> = ({ isSidebarCollaps
         </div>
         <div className='pl-6'>
           <div
-            // onClick={() => setCollapsed(!isCollapsed)}
+            onClick={() => {
+              setCurrentModule(module);
+              setGroupViewType(GroupViewType.INDIVIDUAL_VIEW);
+            }}
             className={
-              'flex grow h-10 w-full px-2 space-x-2 rounded-lg items-center hover:cursor-pointer overflow-clip line-clamp-1' +
-              bg
+              'flex grow h-10 w-full px-2 space-x-2 rounded-b-lg items-center hover:cursor-pointer overflow-clip line-clamp-1' +
+              getGroupViewButtonBackground(GroupViewType.INDIVIDUAL_VIEW)
             }
           >
             <User />
             <h2 className='grow line-clamp-1'>Individual</h2>
           </div>
           <div
-            // onClick={() => setCollapsed(!isCollapsed)}
+            onClick={() => {
+              setCurrentModule(module);
+              setGroupViewType(GroupViewType.GROUP_VIEW);
+            }}
             className={
               'flex grow h-10 w-full px-2 space-x-2 rounded-lg items-center hover:cursor-pointer overflow-clip line-clamp-1' +
-              bg
+              getGroupViewButtonBackground(GroupViewType.GROUP_VIEW)
             }
           >
             <Users />
